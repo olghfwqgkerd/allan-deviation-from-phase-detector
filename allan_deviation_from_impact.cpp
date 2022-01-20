096b53ac0e587cdb834290edbeeb9a1c2e18723f
rho <88160019+95354924@users.noreply.github.com>
2022-01-20 17:29:05 +0100

    Main cpp modify

--- allan_deviation_from_impact.cpp
+++ allan_deviation_from_impact.cpp
@@ -1,35 +1,40 @@
+///////////////////////////////
+//..........or v0.8..........//
+///////////////////////////////
+
 #include<iostream>
 #include<math.h>
 #include<fstream>
 #include<cstdlib>
 #include<tgmath.h>
+#include<stdlib.h>
 
 using namespace std;
 
 fstream input;
 fstream output;
 
-void ConsoleClear(int a)
+void ConsoleClear(int a) //czysczenie konsoli
 {
-  switch(a)
-  {
+    switch (a)
+    {
     case 1: system("cls"); //Windows
-      break;
+        break;
     case 2: system("clear"); //GNU/LINUX
-      break;
-    default: cout << endl << "==================================================" << endl << endl;
-      break;
-  }
+        break;
+    default: cout << endl << "==================================================" << endl << endl; //unknown
+        break;
+    }
 }
 
-//function that counts the sum from the Allan deviation formula
+//funkcja obliczajaca dewiacje Allana
 long double Allan(long long int* arr, int n, const int interval, int samples)
 {
     long double sum = 0;
     for (int i = 0; i < samples - 2 * n; i++)
-    {
-        long double auxSum = 0;
-        auxSum += arr[i + 2 * n] - 2 * arr[i + n] + arr[i];
+    {   // postanowilismy rozpisac wzzor tak aby kazda czesc sumy byla od razu dzielona, aby zmniejszyc ryzyko przpelnienia
+        long double auxSum = 0; //tymcasowa zmienna przechowuajca wynik
+        auxSum += arr[i + 2 * n] - 2 * arr[i + n] + arr[i]; // aktualnie obliczana czesc sumy
         auxSum *= auxSum;
         auxSum /= 2 * n * n;
         auxSum /= interval;
@@ -37,7 +42,7 @@ long double Allan(long long int* arr, int n, const int interval, int samples)
         auxSum /= (samples - 2 * n);
         sum += auxSum;
     }
-    return sqrt(sum);
+    return sqrt(sum); //zwracanie calej sumy przez pierwiastek
 }
 
 int main()
@@ -45,87 +50,98 @@ int main()
     int system = 0;
     char choose = 'a';
     int samples = 90000; //N
-    int constComp = 50000; //Constant component
+    int constComp = 50000; //sk³adowa sta³a
     int interval = 10000000; //t0
+    int measRangVal = 100000; //Measuring range value
 
+    cout << endl;
     cout << "     (1) Windows" << endl;
     cout << "     (2) GNU/Linux" << endl;
     cout << "     Choose operating system: ";
     cin >> system;
 
-    while(choose != 's' && choose != 'S')
+    while (choose != 's' && choose != 'S')
     {
-      ConsoleClear(system);
+        ConsoleClear(system);
+        cout << endl;
 
-      cout << " (0) Operating system ............ ";
-      switch(system)
-      {
-        case 1: cout << "Windows" << endl;
-          break;
-        case 2: cout << "GNU/Linux" << endl;
-          break;
-        default: cout << "unknown" << endl;
-          break;
-      }
-      cout << "     File path ................... " << "/putty_example_1.log" << endl;
-      cout << " (2) Number of samples (N) ....... " << samples << endl;
-      cout << " (3) Constant component .......... "<< constComp << endl;
-      cout << "     Type of wave ................ " << "Sawtooth wave" << endl;
-      cout << " (5) Interval (t0) ............... " << interval << endl;
-      cout << endl;
-      cout << "     Choose number to edit or (S)tart (E)xit ";
-
-      cin >> choose;
-
-      if(choose != 's' && choose != 'S')
-      {
-        ConsoleClear(9);
-        switch(choose)
+        cout << " (0) Operating system ............ ";
+        switch (system)
         {
-          case '0':
-          cout << "     (1) Windows" << endl;
-          cout << "     (2) GNU/Linux" << endl;
-          cout << "     Choose operating system: ";
-          cin >> system;
+        case 1: cout << "Windows" << endl;
             break;
-          case '2':
-              cout << "Number of samples: ";
-              cin >> samples;
+        case 2: cout << "GNU/Linux" << endl;
             break;
-          case '3':
-              cout << "Constant component: ";
-              cin >> constComp;
+        default: cout << "unknown" << endl;
             break;
-          case '5':
-              cout << "Interval: ";
-              cin >> interval;
-            break;
-          default: break;
         }
-      }
+        cout << "     Input file path ............. " << "./putty_example_1.log" << endl;
+        cout << "     Output file path ............ " << "./output.txt" << endl;
+        cout << " (3) Number of samples (N) ....... " << samples << endl;
+        cout << " (4) Constant component .......... " << constComp << endl;
+        cout << "     Type of wave ................ " << "Sawtooth wave" << endl;
+        cout << " (6) Interval (t0) ............... " << interval << endl;
+        cout << " (7) Measuring range value ....... " << measRangVal << endl;
+        
+        cout << endl;
+        cout << "     Choose number to edit or (S)tart (E)xit ";
+
+        cin >> choose;
+
+        if (choose == 'e' || choose == 'E') exit(0);
+
+        if (choose != 's' && choose != 'S')
+        {
+            ConsoleClear(9);
+            switch (choose)
+            {
+            case '0':
+                cout << "     (1) Windows" << endl;
+                cout << "     (2) GNU/Linux" << endl;
+                cout << "     Choose operating system: ";
+                cin >> system;
+                break;
+            case '3':
+                cout << "Number of samples: ";
+                cin >> samples;
+                break;
+            case '4':
+                cout << "Constant component: ";
+                cin >> constComp;
+                break;
+            case '6':
+                cout << "Interval: ";
+                cin >> interval;
+                break;
+            case '7':
+                cout << "Measuring range value: ";
+                cin >> measRangVal;
+                break;
+            default: break;
+            }
+        }
     }
 
     ConsoleClear(system);
 
-    long long int* container = new long long int[samples];
+    long long int* container = new long long int[samples]; // tworzenie tabeli do przechowywania zmiennych z pliku
     long long int x = 0;
     bool check = false;
 
-    input.open( "putty_example_1.log", ios::in);
+    input.open("putty_example_1.log", ios::in);
     output.open("output.txt", ios::out);
 
     for (int i = 0; i < samples; i++)
     {
-        //cout << container[i] << endl;
         input >> container[i];
-        container[i] -= constComp; //removal of constant component
+        container[i] -= constComp; //usuniecie skladowej zmiennej
 
-        if (container[i] < 0)
+        if (container[i] < 0) // wykrywanie przeskoku
         {
             if (!check)
             {
                 check = true;
-                x++;
+                x++; // zliczanie kolejnych przeskokow
             }
         }
         else if (check)
@@ -133,7 +149,7 @@ int main()
             check = false;
         }
 
-        container[i] += 100000 * x;
+        container[i] += measRangVal * x; //dodanie wartoœci zakresu pomiaroweg
         container[i] *= 10; //[ns]
     }
 
@@ -143,27 +159,27 @@ int main()
     int loadingStatus = 0;
     long double allanTemp;
 
-    cout << "--------Start of the calculation--------" << endl << endl;
+    cout << endl << "--------Start of the calculation--------" << endl << endl;
 
     for (int n = 3; n < samples / 4; n++)
     {
         allanTemp = Allan(container, n, interval, samples);
-        onePercent = ( ( samples / 4 ) - 1 )/ 100;
+        onePercent = ((samples / 4) - 1) / 100;
 
-        if(detect == onePercent)
+        if (detect == onePercent)
         {
-          cout << loadingStatus << " %";
-          if( loadingStatus % 10 == 0)
-          {
-            cout << "\t ...random checks: " << allanTemp << endl;
-          }
-          else cout << endl;
-          detect = 3;
-          loadingStatus ++;
+            cout << "     " << loadingStatus << "%";
+            if (loadingStatus % 10 == 0)
+            {
+                cout << "     ...random checks: " << allanTemp << endl;
+            }
+            else cout << endl;
+            detect = 3;
+            loadingStatus++;
         }
         else detect++;
 
-        output << n << "\t" << allanTemp << endl;                                               //save the outputs of the calculations for n to a file...
+        output << n << "\t" << allanTemp << endl; // zapisanie wynikow do pliku
     }
 
     input.close();

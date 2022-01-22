d63f7dd06b05f49e16aeef05398e65778bfa0b10
95354924 <xq29ypr86yao@tutanota.com>
2022-01-22 21:28:41 +0100

    Reorganise

--- allan-deviation-from-phase-detector.cpp
+++ allan-deviation-from-phase-detector.cpp
@@ -1,93 +1,110 @@
-////////////////////////////////////
-//..........olg rog v0.8..........//
-////////////////////////////////////
-
+////////////////////////////////////////////////////////////////////////////////
+//or.oyeaiu...22-01-2022......................................................//
+////////////////////////////////////////////////////////////////////////////////
 #include<iostream>
 #include<fstream>
-#include<math.h>
+#include<math.h> // sqrt
+#include<cstdlib> // exit
 
 using namespace std;
 
-fstream input;
-fstream output;
-
-void TerminalClear(int a) // czysczenie terminalu
+void TerminalClear(int a)
 {
-    switch (a)
-    {
-    case 1: system("cls"); // Windows
+	switch (a)
+	{
+    case 1:
+		cout << endl;
+		system("cls"); // Windows
         break;
-    case 2: system("clear"); // GNU/LINUX
+    case 2:
+		cout << endl;
+		system("clear"); // Unix
         break;
-    default: cout << endl << "==================================================" << endl << endl; //unknown
+    default: cout << endl << "===========================================================" << endl << endl; //unknown
         break;
     }
 }
 
-// funkcja obliczajaca dewiacje Allana
-long double Allan(long long int* arr, int n, const int interval, int samples)
+
+long double AllanDeviation(long long int* arr, unsigned int n, unsigned int interval, unsigned int samples)
 {
     long double sum = 0;
-    for (int i = 0; i < samples - 2 * n; i++)
-    {   // postanowilismy rozpisac wzzor tak aby kazda czesc sumy byla od razu dzielona, aby zmniejszyc ryzyko przpelnienia
-        long double auxSum = 0; // tymcasowa zmienna przechowuajca wynik
-        auxSum += arr[i + 2 * n] - 2 * arr[i + n] + arr[i]; // aktualnie obliczana czesc sumy
-        auxSum *= auxSum;
-        auxSum /= 2 * n * n;
-        auxSum /= interval;
-        auxSum /= interval;
-        auxSum /= (samples - 2 * n);
-        sum += auxSum;
+    for (unsigned int i = 0; i < samples - 2 * n; i++)
+    {
+        long double tempSum = 0;
+        tempSum += arr[i + 2 * n] - 2 * arr[i + n] + arr[i];
+        tempSum *= tempSum;
+        tempSum /= 2 * n * n;
+        tempSum /= interval;
+        tempSum /= interval;
+        tempSum /= (samples - 2 * n);
+        sum += tempSum;
     }
-    return sqrt(sum); // zwracanie calej sumy przez pierwiastek
+    return sqrt(sum);
+}
+
+int SystemSelection()
+{
+	int a = 3;
+    cout << "(1) Windows" << endl;
+    cout << "(2) GNU/Linux or MacOS" << endl;
+	cout << "(3) Unknown" << endl;
+    cout << "    Select operating system: ";
+    cin >> a;
+	return a;
 }
 
 int main()
 {
+    fstream input;
+    fstream output;
+
     int system = 0;
     char choose = 'x';
-    int samples = 90000; // liczba probek
-    int constComp = 50000; // składowa stała
-    int interval = 10000000; // t0
-    int measRangVal = 100000; // wartosc zakresu pomiarowego
 
-    cout << endl;
-    cout << "     (1) Windows" << endl;
-    cout << "     (2) GNU/Linux" << endl;
-    cout << "     Choose operating system: ";
-    cin >> system;
+    unsigned int samples = 90000; // number of samples, records in logs
+    float offset = 50000; // offset
+    unsigned int interval = 10000000; // t0
+    unsigned int measRangVal = 100000; // measuring range value
+
+	system = SystemSelection();
+
+//##################### MENU SECTION #########################
 
     while (choose != 's' && choose != 'S')
     {
         TerminalClear(system);
-        cout << endl;
 
-//######################### MENU #########################
-
-        cout << " (0) Operating system ............ ";
+		cout << "====================[PARAMETER SETTING]====================" << endl;
+        cout << "(0) Operating system ............... ";
         switch (system)
         {
         case 1: cout << "Windows" << endl;
             break;
-        case 2: cout << "GNU/Linux" << endl;
+        case 2: cout << "GNU/Linux or MacOS" << endl;
             break;
         default: cout << "unknown" << endl;
             break;
         }
-        cout << "     Input file path ............. " << "./putty_example_1.log" << endl;
-        cout << "     Output file path ............ " << "./output.txt" << endl;
-        cout << " (3) Number of samples (N) ....... " << samples << endl;
-        cout << " (4) Constant component .......... " << constComp << endl;
-        cout << "     Type of wave ................ " << "Sawtooth wave" << endl;
-        cout << " (6) Interval (t0) ............... " << interval << endl;
-        cout << " (7) Measuring range value ....... " << measRangVal << endl;
-
+        cout << "    Input file path ................ " << "./input.log" << endl;
+        cout << "    Output file path ............... " << "./output.txt" << endl;
+		cout << "-----------------------------------------------------------" << endl;
+		cout << "    Type of wave ................... " << "Sawtooth wave" << endl;
+        cout << "(4) Number of samples [N] .......... " << samples << endl;
+        cout << "(5) Offset ......................... " << offset << endl;
+        cout << "(6) Interval [t0] .................. " << interval << endl;
+        cout << "(7) Measuring range value .......... " << measRangVal << endl;
         cout << endl;
-        cout << "     Choose number to edit or (S)tart (E)xit ";
+        cout << "    Choose number to edit, (S)tart or (E)xit ";
 
         cin >> choose;
 
-        if (choose == 'e' || choose == 'E') exit(0);
+        if (choose == 'e' || choose == 'E')
+		{
+			TerminalClear(system);
+			cout << "Program ended...";
+			exit(0);
+		}
 
         if (choose != 's' && choose != 'S')
         {
@@ -95,18 +112,15 @@ int main()
             switch (choose)
             {
             case '0':
-                cout << "     (1) Windows" << endl;
-                cout << "     (2) GNU/Linux" << endl;
-                cout << "     Choose operating system: ";
-                cin >> system;
+				system = SystemSelection();
                 break;
-            case '3':
+            case '4':
                 cout << "Number of samples: ";
                 cin >> samples;
                 break;
-            case '4':
-                cout << "Constant component: ";
-                cin >> constComp;
+            case '5':
+                cout << "Offset: ";
+                cin >> offset;
                 break;
             case '6':
                 cout << "Interval: ";
@@ -116,60 +130,59 @@ int main()
                 cout << "Measuring range value: ";
                 cin >> measRangVal;
                 break;
-            default: break;
+            default:
+				break;
             }
         }
     }
 
     TerminalClear(system);
 
-    long long int* container = new long long int[samples]; // tworzenie tabeli do przechowywania zmiennych z pliku
-    long long int x = 0;
+    long long int* container = new long long int[samples];
+	long long int x = 0;
     bool check = false;
 
-    input.open("putty_example_1.log", ios::in);
+    input.open("putty.log", ios::in);
     output.open("output.txt", ios::out);
 
-    for (int i = 0; i < samples; i++)
+    for (unsigned int i = 0; i < samples; i++)
     {
         input >> container[i];
-        container[i] -= constComp; // usuniecie skladowej zmiennej
-
-        if (container[i] < 0) // wykrywanie przeskoku
+        container[i] -= offset;
+        if (container[i] < 0)
         {
             if (!check)
             {
                 check = true;
-                x++; // zliczanie kolejnych przeskokow
+                x++;
             }
         }
         else if (check)
         {
             check = false;
         }
-
-        container[i] += measRangVal * x; // dodanie wartości zakresu pomiaroweg
-        container[i] *= 10; //[ns]
+        container[i] += measRangVal * x;
+        container[i] *= 10;
     }
 
     int onePercent;
     int detect = 0;
     int loadingStatus = 0;
-    long double allanTemp;
+    long double allanTemp = 0;
 
-    cout << "--------Start of the calculation--------" << endl << endl;
+    cout << "=================[Start of the calculation]================" << endl << endl;
 
-    for (int n = 3; n < samples / 4; n++)
+    for (unsigned int n = 3; n < samples / 4; n++)
     {
-        allanTemp = Allan(container, n, interval, samples);
+        allanTemp = AllanDeviation(container, n, interval, samples);
         onePercent = ((samples / 4) - 1) / 100;
 
         if (detect == onePercent)
         {
-            cout << "     " << loadingStatus << "%";
+            cout << loadingStatus << "%";
             if (loadingStatus % 10 == 0)
             {
-                cout << "     ...random checks: " << allanTemp << endl;
+                cout << "........random checks: " << allanTemp << endl;
             }
             else cout << endl;
             detect = 3;
@@ -177,12 +190,12 @@ int main()
         }
         else detect++;
 
-        output << n << "\t" << allanTemp << endl; // zapisanie wynikow do pliku
+        output << n << "\t" << allanTemp << endl;
     }
 
     input.close();
     output.close();
 
-    cout << endl << "...opus magnum, its done.";
+    cout << endl << "...opus magnum, its done. Program ended...";
     return 0;
 }
